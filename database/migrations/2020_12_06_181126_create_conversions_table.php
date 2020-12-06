1a25d4e590da17e4a3134f27eef43ade5e02d32e
Michal Brzozowski <contact@michalbrzozowski.com>
2020-12-06 19:14:32 +0100

    Add conversion model and migration

---
+++ database/migrations/2020_12_06_181126_create_conversions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("offer_id");
+            $table->string("offer_name");
+            $table->string("aff_sub");
+            $table->string("aff_sub2");
+            $table->string("aff_sub3");
+            $table->string("aff_sub4");
+            $table->string("aff_sub5");
+            $table->string("session_ip");
+            $table->float("payout");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversions');
+    }
+}
